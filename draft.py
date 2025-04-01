3cc0613dd29d44afbda74ed714a7b3e30d2e9494
rt.kinsonli <kinsonli@rayootech.com>
2025-04-01 17:01:05 +0800

    def

--- draft.py
+++ draft.py
@@ -20,6 +20,8 @@ class Test:
         self.script = draft.Script_file(1920, 1080)
         self.script.add_track(draft.Track_type.audio, 'TTS')
         self.script.add_track(draft.Track_type.audio, 'BGM')
+        self.script.add_track(draft.Track_type.text, 'T0')
+        
         audio_bgm = draft.Audio_material(os.path.join(self.tutorial_asset_dir, bgm))
         audio_bgm_lenth = audio_bgm.duration/1000000
         self.script.add_material(audio_bgm)
@@ -28,7 +30,7 @@ class Test:
         self.script.add_segment(audio_bgm_segment, 'BGM')
         
     def addTitle(self, filename: str):
-        audio_material = draft.Audio_material(os.path.join(self.tutorial_asset_dir, 'title.mp3'))
+        audio_material = draft.Audio_material(os.path.join(self.tutorial_asset_dir, f'{filename}.mp3'))
         audio_duration = audio_material.duration/1000000
         self.script.add_material(audio_material)
         audio_segment = draft.Audio_segment(audio_material,
@@ -36,10 +38,17 @@ class Test:
                                     volume=1)
         self.script.add_segment(audio_segment, 'TTS')
         self.nowS += audio_duration + 0.3
+        #字幕
+        text_segment = draft.Text_segment("千古词帝，李煜的巅峰之作", trange(0, self.script.duration),  # 文本将持续整个视频（注意script.duration在上方片段添加到轨道后才会自动更新）
+                                  font=draft.Font_type.文轩体,                                  # 设置字体为文轩体
+                                  style=draft.Text_style(color=(1.0, 1.0, 0.0)),                # 设置字体颜色为黄色
+                                  clip_settings=draft.Clip_settings(transform_y=-0.8))          # 模拟字幕的位置
+        self.script.add_segment(text_segment, 'T0')
         
     
     def addItem(self, filename: str) -> str:
-        audio_material = draft.Audio_material(os.path.join(self.tutorial_asset_dir, filename))
+        # 音频
+        audio_material = draft.Audio_material(os.path.join(self.tutorial_asset_dir, f'{filename}.mp3'))
         audio_duration = audio_material.duration/1000000
         self.script.add_material(audio_material)
         audio_segment = draft.Audio_segment(audio_material,
@@ -47,6 +56,7 @@ class Test:
                                     volume=1)
         self.script.add_segment(audio_segment, 'TTS')
         self.nowS += audio_duration + 0.3
+        # 字幕
         
         return 'Success'
 
@@ -64,7 +74,12 @@ class Test:
     #             self.addAudio(filename)
 if __name__ == "__main__":
     testObj = Test('爱的供养-间奏.mp3')
-    testObj.addTitle('title.mp3')
-    testObj.addItem('a.mp3')
-    testObj.addItem('b.mp3')
+    testObj.addTitle('t0')
+    testObj.addItem('t1')
+    testObj.addItem('t2')
+    testObj.addItem('t3')
+    testObj.addItem('t4')
+    testObj.addItem('t5')
+    testObj.addItem('t6')
+    testObj.addItem('t7')
     testObj.dumpDraft()
